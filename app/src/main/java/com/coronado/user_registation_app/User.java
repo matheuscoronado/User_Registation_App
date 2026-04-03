88c539b7a97e29811671eb30feb2503220fd547f
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-02 21:31:03 -0300

    Criado os metodos getter e setter

--- app/src/main/java/com/coronado/user_registation_app/User.java
+++ app/src/main/java/com/coronado/user_registation_app/User.java
@@ -16,4 +16,40 @@ public class User {
     private String cpf; // CPF do usuário
     private String email;  // Email do usuário
     private String phone; // Fone do usuário
+
+    // Construtor da classe que será usado para criar novo objeto
+    public User(String name, String cpf, String email, String phone){
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Metodos getter e setter para acessar e modificar os dados do objeto User
+    public int getId(){return id;}
+
+    // Define o ID do usuário (usado pelo Room para preencher automaticamente)
+    public void setId(int id) {this.id = id;}
+
+
+    // Retorna o nome do usuário
+    public String getName(){return name;}
+
+    // Retorna o cpf do usuário
+    public String getCpf(){return cpf;}
+
+    // Retorna o email do usuário
+    public String getEmail(){return email;}
+
+    // Retorna o fone do usuário
+    public String getPhone(){return phone;}
+
+
+    public void setName(String name) {this.name = name;}
+
+    public void setCpf(String cpf) {this.cpf = cpf;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setPhone(String phone) {this.phone = phone;}
 }
