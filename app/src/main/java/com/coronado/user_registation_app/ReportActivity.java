bb7f8b94259808ae49e8607c250949dc64dd2c8f
Matheus Coronado <matheuscoronado48@gmail.com>
2026-04-09 19:59:54 -0300

    Criado campo onde os dados do banco serão exibidos

--- app/src/main/java/com/coronado/user_registation_app/ReportActivity.java
+++ app/src/main/java/com/coronado/user_registation_app/ReportActivity.java
@@ -1,4 +1,35 @@
 package com.coronado.user_registation_app;
 
-public class ReportActivity {
+//Importações de componentes de UI, intenções e a biblioteca ROOM
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ReportActivity extends AppCompatActivity {
+    //Campo de texto onde os dados do banco serão exibidos
+    private TextView textViewReport;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        // Define o layout XML dessa tela de relatorio
+        setContentView(R.layout.activity_report);
+        // Mapeamento do TextView do XML para o java
+        textViewReport = findViewById(R.id.textViewReport);
+        // Encontra o botão e define o clique para voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        // O botão de retorno utilizando expressão lambda
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+    }
+
+    // Metodo responsavel pela navegação entre as telas do app
+    public void voltarParaCadastro(){
+        // Inteção para abrir tela de cadastro
+        Intent intent = new Intent(ReportActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Fecha a tela de relatorio para não acumular na pilha de tarefa
+    }
 }
